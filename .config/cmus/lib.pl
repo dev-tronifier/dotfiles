d875579629c62fac78faad6c3fdf4b857f9b8699
dev-tronifier <dev.tronifier@gmail.com>
2021-08-31 23:01:50 +0530

    .

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,104 @@
+/home/tronifier/music/''Soul Funk'' 90s Instrumental Boom Bap Hip Hop Beat - Prod.By Isqo-3PO1kF-y26Y.mp3
+/home/tronifier/music/'Kabira Full Song' Yeh Jawaani Hai Deewani _ Pritam _ Ranbir Kapoor, Deepika Padukone-jHNNMj5bNQw.mp3
+/home/tronifier/music/'The Get Back' [FREE] j cole type beat _ the off season type beat 2021-K1ueJ466l6s.mp3
+/home/tronifier/music/(FREE) 6lack Type Beat - 'I Can Be Lonely'-y2OsiWkaIac.mp3
+/home/tronifier/music/(FREE) Drake x Roddy Ricch x Wheezy Type Beat - 'Win' (Prod. Gibbo x Ferno)-8JK6ArZm-5o.mp3
+/home/tronifier/music/(FREE) Lofi Type Beat - 90's Freestyle (prod. Yusei)-tEu9kZLGx_E.mp3
+/home/tronifier/music/(FREE) Wu Tang Clan x Mobb Deep Old School Type Beat [2020] - Bonafide-7qU8-ZyMxVg.mp3
+/home/tronifier/music/(SOLD) MF DOOM x Mac Miller Type Beat [2020] - Slow Motion-jSXtc5eIyec.mp3
+/home/tronifier/music/(SOLD) SZA x Bryson Tiller Type Beat - But you were not the one for me (prod. Yusei)---SBFQ1t5Ow.mp3
+/home/tronifier/music/1 Hour Of Beats [FREE] kendrick lamar, j cole, jid, kenny mason, smino type beats 2021-jjRLOgE2Jn0.mp3
+/home/tronifier/music/A$AP ROCKY X KENDRICK LAMAR TYPE BEAT 'CARSTOPPER'--zzMpbqUbXU.mp3
+/home/tronifier/music/AViVA - Blame It On The Kids-Z9lKN_X152Q.mp3
+/home/tronifier/music/AViVA - Psycho-abAfJ-sjW88.mp3
+/home/tronifier/music/AViVA - QUEEN OF THE FREAKS [Lyrics]-Nu56cTqohlM.mp3
+/home/tronifier/music/AViVA - Rabbit Hole-g61L7Y8bkwo.mp3
+/home/tronifier/music/Allie X - Paper Love-PbBFFCmrtRw.mp3
+/home/tronifier/music/Amber Run - I Found-Yj6V_a1-EUA.mp3
+/home/tronifier/music/BROWN MUNDE - AP DHILLON _ GURINDER GILL _ SHINDA KAHLON _ GMINXR-VNs_cCtdbPc.mp3
+/home/tronifier/music/Billie Eilish - Bury A Friend (Zeds Dead Remix)-KvmNDnFnW7o.mp3
+/home/tronifier/music/Billie Eilish, Khalid - lovely-V1Pl8CzNzCw.mp3
+/home/tronifier/music/Blue Boy - Remember Me (Original 12')-fKKNPLowteY.mp3
+/home/tronifier/music/Bryce Fox - Horns (STéLOUSE Remix)-Q_NB5luxtic.mp3
+/home/tronifier/music/CHROMANCE - Wrap Me In Plastic (Marcus Layton Edit) [Lyrics]-d9j92rVDbTE.mp3
+/home/tronifier/music/Cigarettes After Sex - Nothing's Gonna Hurt You Baby-R2LQdh42neg.mp3
+/home/tronifier/music/DIVINE - Kaam 25 _ Sacred Games (Prod. by Phenom)-NU3xm6N7tzw.mp3
+/home/tronifier/music/Damn _ Raftaar Ft. KR$NA _ Mr. Nair-yBRKqRc-vyQ.mp3
+/home/tronifier/music/Desiigner - Panda (Official Music Video)-E5ONTXHS2mM.mp3
+/home/tronifier/music/Dhee ft. Arivu - Enjoy Enjaami (Prod. Santhosh Narayanan)-eYq7WapuDLU.mp3
+/home/tronifier/music/Drake - 0 to 100 _ The Catch Up Instrumental-HBntk3w-ExA.mp3
+/home/tronifier/music/EMIWAY BANTAI - PAANI MAI _ SWAALINA _ YOUNG GALIB _ (OFFICIAL MUSIC VIDEO)-uYoPUKFM2VE.mp3
+/home/tronifier/music/FKJ & Masego - Tadow-hC8CH0Z3L54.mp3
+/home/tronifier/music/FOTTY SEVEN - BAAP SE ft. KR$NA _ ASLI INDEPENDENT EP _ KALAMKAAR-i_whG7S0lDc.mp3
+/home/tronifier/music/FREEVERSE FEAST (LANGAR)  - KR$NA _ Music By RAFTAAR _ KALAMKAAR-z0yeFVUs-70.mp3
+/home/tronifier/music/FREE_ Dave East x Rick Ross Type Beat ~ 'Heights' (Prod. 1O1)-00GlSwklGf4.mp3
+/home/tronifier/music/Flight Facilities - Clair De Lune feat. Christine Hoberg-Jcu1AHaTchM.mp3
+/home/tronifier/music/Free J Cole x Erykah Badu Type Beat - Flow-7oNf1doKi68.mp3
+/home/tronifier/music/GOAT (Full Video) Sidhu Moose Wala _ Wazir Patar _ Sukh Sanghera _ Moosetape-M8vDwlHigJA.mp3
+/home/tronifier/music/Gaullin - Moonlight-UvjErrYBT4Y.mp3
+/home/tronifier/music/HYPNOTIZE _ OutKast_Nas_J Dilla Type Beat _ [Prod. VYLAN]-r5CA_Plt9Z4.mp3
+/home/tronifier/music/Isaiah Rashad x Mick Jenkins Type Beat 'Around The Way' _ [NEW 2020]-H1AlWeMimcM.mp3
+/home/tronifier/music/J Cole Type Beat 'House of lies' _ [NEW 2020]-ZKLNNTAysuU.mp3
+/home/tronifier/music/Jynjo - How do you Like it (feat. Spritely)-NC-uaFbQgeo.mp3
+/home/tronifier/music/KARMA - 1 Se 23 _ Mic Check - Season 1 _ Episode 7 _ AK Projekts-5Xxvs_itUSk.mp3
+/home/tronifier/music/KAUN TUJHE Full  Video _ M.S. DHONI -THE UNTOLD STORY _Amaal Mallik Palak_Sushant Singh Disha Patani-atVof3pjT-I.mp3
+/home/tronifier/music/KR$NA  - MAKASAM _ KALAMKAAR-cUfTYaVEnoA.mp3
+/home/tronifier/music/KR$NA - Fall Off (Extended) _ Official Music Video-lQy2e-aWTfY.mp3
+/home/tronifier/music/KR$NA - MAHARANI (FULL VIDEO) _ KALAMKAAR-KPEJDCFe3-A.mp3
+/home/tronifier/music/KR$NA - SAY MY NAME _ HINDI VERSION _ KALAMKAAR-vRIJaQsJts8.mp3
+/home/tronifier/music/KR$NA - SEEDHA MAKEOVER _ KALAMKAAR-fHSo2cpyIas.mp3
+/home/tronifier/music/KR$NA - UNTITLED (FULL VIDEO) _ KALAMKAAR-2KsrtJ3VtZs.mp3
+/home/tronifier/music/KR$NA Ft. Karma & IKKA - Villain _ Official Video-aexf8V5LDhc.mp3
+/home/tronifier/music/KR$NA Ft. RAFTAAR  - Saza-E-Maut _ Official Music Video _ (Indian Drill)-o907r6NsK9s.mp3
+/home/tronifier/music/KR$NA X RAFTAAR - SAATH YA KHILAAF _ KALAMKAAR-RdLP3eQ__po.mp3
+/home/tronifier/music/Kadhalar Dhinam - Dhaandiya Song-sK14fTRfnsQ.mp3
+/home/tronifier/music/Kanavae Kanavae Official Video Song HD-Q-_cu_78eIA.mp3
+/home/tronifier/music/Kendrick Lamar x ScHoolboy Q Type Beat 'Patiently Waiting' _ [NEW 2019]-WVtbgxUgJOI.mp3
+/home/tronifier/music/King - Tu Aake Dekhle _ The Carnival _ The Last Ride _ Prod. by Shahbeatz _ Latest Hit Songs 2020-A66TYFdz8YA.mp3
+/home/tronifier/music/Krewella - Calm Down (SKAN Remix)-idPqijh2EWk.mp3
+/home/tronifier/music/LAYTO - Little Poor Me-sVUjuHvs1OA.mp3
+/home/tronifier/music/Lil Wayne, Wiz Khalifa & Imagine Dragons w_ Logic & Ty Dolla $ign ft X Ambassadors - Sucker for Pain--59jGD4WrmE.mp3
+/home/tronifier/music/Lilianna Wilde - Grind Me Down (Jawster Remix)-iHUWUXlr-CM.mp3
+/home/tronifier/music/MISSIO - Everybody Gets High (Audio)-AHukwv_VX9A.mp3
+/home/tronifier/music/Mac Miller - Self Care (Official Music Video)-SsKT0s5J8ko.mp3
+/home/tronifier/music/Madharasapattinam - Pookkal Pookkum Video _ Aarya, Amy Jackson-nWxGhq_lBII.mp3
+/home/tronifier/music/Milky Chance - Stolen Dance (Official Video)-iX-QaNzd-0Y.mp3
+/home/tronifier/music/Mos Def - Respiration (Instrumentals)-fHHSpfssBco.mp3
+/home/tronifier/music/NEFFEX - Fight Back-6vfP_4u7zik.mp3
+/home/tronifier/music/Naam - Adi Penne (Duet) Official Video [4K] - T Suriavelan _ Rupiny _ Stephen Zechariah ft Srinisha-2pOVz45p8Ho.mp3
+/home/tronifier/music/No Competition  - Jass Manak Ft DIVINE (Full Video) Satti Dhillon _ New Songs _ GK DIGITAL _ Geet MP3-bNtOvlT9ZCQ.mp3
+/home/tronifier/music/OLD SKOOL (Full Video) Prem Dhillon ft Sidhu Moose Wala _ The Kidd _ Nseeb _ Rahul Chahal _GoldMedia-hBlO1i_WTiY.mp3
+/home/tronifier/music/RAFTAAR - BLACK SHEEP _ BAR'ISH EP _ Official Music Video-QCxXJMhQNZQ.mp3
+/home/tronifier/music/Raataan Lambiyan – Official Video _ Shershaah _ Sidharth – Kiara _ Tanishk B_ Jubin Nautiyal  _Asees-gvyUuxdRdR4.mp3
+/home/tronifier/music/Raftaar x Brodha V - Naachne Ka Shaunq-QvswgfLDuPg.mp3
+/home/tronifier/music/Seafret - Oceans-aqsL0QQaSP4.mp3
+/home/tronifier/music/Seedhe Maut - 'Nanchaku' ft MC STΔN _ Azadi Records-3beb3HXqCms.mp3
+/home/tronifier/music/Shaefri - Give It All (Official Video)-WHX1iILBrZk.mp3
+/home/tronifier/music/Shaefri - Monster (Official Video)-dArqKW2NZd8.mp3
+/home/tronifier/music/Shaefri - Pixelate (Official Video)-qLqTnSskwzQ.mp3
+/home/tronifier/music/Shaefri - Rabbit Hole (Official Video)-b5JWMqJlZrA.mp3
+/home/tronifier/music/She Move It Like - Official Video _ Badshah _ Warina Hussain _ ONE Album-Ci0WbaUH3no.mp3
+/home/tronifier/music/Sickick - Mind Games-c-EkFgViaPE.mp3
+/home/tronifier/music/Sociopath-COdNIBdIMjk.mp3
+/home/tronifier/music/Tenu Na Bol Pawaan - Behen Hogi Teri _ Shruti Haasan,Raj Kummar Rao _ Yasser Desai _ Amjad Nadeem-yEJQpoHfw0s.mp3
+/home/tronifier/music/The Kooks - Bad Habit-3tUh-x-fp8Q.mp3
+/home/tronifier/music/Thodi Der -Full Video _ Half Girlfriend _ Arjun Kapoor & Shraddha Kapoor _ Farhan S & Shreya Ghoshal-FGTv9-oQhIg.mp3
+/home/tronifier/music/Top 20-6 LOOPSTATION (Solo) Wildcard Compilation _ GBB21 - WORLD LEAGUE-7mxhF4QED8c.mp3
+/home/tronifier/music/Tove Lo - Habits (Stay High) - Hippie Sabotage Remix-SYM-RJwSGQ8.mp3
+/home/tronifier/music/UNDREAM - Nightmare (ft. Neoni)-zFpLFW3lfDs.mp3
+/home/tronifier/music/Unlike Pluto - Revenge, And A Little More-A0T6LwpJGxw.mp3
+/home/tronifier/music/Unlike Pluto ft. Mike Taylor - Everything Black-uXRLanZp2sk.mp3
+/home/tronifier/music/Vaaranam Aayiram - Mundhinam Video _ Harris Jayaraj _ Suriya-93iK4wNij8E.mp3
+/home/tronifier/music/Vanic X Zella Day - Hypnotic-O8K2QRwKq54.mp3
+/home/tronifier/music/XYLØ - Yes & No-kkKqQhnGtOc.mp3
+/home/tronifier/music/Yas - Empty Crown-m_qlgFQs7E4.mp3
+/home/tronifier/music/[FREE] A$AP Rocky x 21 Savage type beat ''crook''-GqmJTuKKbTE.mp3
+/home/tronifier/music/[FREE] Isaiah Rashad x Kendrick Lamar x J. Cole Chill Type Beat _ 'Telesto'-kSGysR9yD0I.mp3
+/home/tronifier/music/[FREE] J. Cole x Isaiah Rashad x Kendrick Lamar Type Beat _ 'Triton'-tGpypPiPbVE.mp3
+/home/tronifier/music/[FREE] Mac Miller Type Beat 'Smoke Break'-zrwz_b966rs.mp3
+/home/tronifier/music/[FREE] Mac Miller Type Beat 'When in Rome'-sPABM7L5oJ0.mp3
+/home/tronifier/music/[FREE] The Notorious B.I.G x 2Pac type beat _ 90's Gangsta rap type beat _ East Coast_West Coast-dFgpxRFetgI.mp3
+/home/tronifier/music/alt-J (∆) Breezeblocks-rVeMiVU77wo.mp3
+/home/tronifier/music/boerd - Blind-wc4sKNJJ5Hk.mp3
+/home/tronifier/music/june - A Little Messed Up-IHoj7qvNmwk.mp3
+/home/tronifier/music/n u a g e s - Dreams ❀-9RMHHwJ9Eqk.mp3
---
+++ .vim/templates/_skelton.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+//[{{{{close
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vb = vector < bool >;
+using vvb = vector < vb >;
+using vd = vector < double >;
+using vs = vector < string >;
+
+using pii = pair < int, int >;
+using pll = pair < ll, ll >;
+using pdd = pair < double, double >;
+
+using vpii = vector < pii >;
+using vpll = vector < pll >;
+using vpdd = vector < pdd >;
+//}}}}]close
+int main() {
+
+	return 0;
+}
+
